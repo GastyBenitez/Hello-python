8f7bb9437ec5002be372e87c84fa574c3e06c285
GastyBenitez <gastonbenitez0207@gmail.com>
2026-06-25 05:25:12 -0300

    inicio

---
+++ 00_hello.py
@@ -0,0 +1,29 @@
+# Esto es un comentario
+#Nuestro Hola mundo en python
+print("Hola Python")
+print('Hola Python')
+
+
+
+"""
+Este es un 
+comentario
+en varias lineas
+
+"""
+
+
+'''
+Este tambien es un 
+comentario 
+en varias lineas
+'''
+
+# Consultar el tipo de dato
+print(type("Soy un dato srt")) # Tipo 'str'
+print(type(5)) # Tipo 'int'
+print(type(1.5)) # Tipo de dato 'float'
+print(type(1 + 1j))  # Tipo de dato 'complex'
+print(type(True)) #Tipo de dato 'bool'
+
+
